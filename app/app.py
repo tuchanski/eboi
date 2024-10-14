31d7cca9851b70c884f20ccf454359b8b780ca8f
Tuchanski <gui.rocha2004@gmail.com>
2024-10-14 15:56:58 -0300

    Corrige bug do campo de login

--- app/app.py
+++ app/app.py
@@ -13,7 +13,7 @@ app.secret_key = os.urandom(24)
 DB_HOST = "localhost"
 DB_NAME = "eBoi"
 DB_USER  = "postgres"
-DB_PASS = "postgres"
+DB_PASS = "root" # MUDE CONFORME A SUA MÁQUINA
 DB_PORT = "5432"
 
 conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST, port=DB_PORT)
@@ -39,7 +39,7 @@ def login():
     if request.method == "POST":
         cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
         email = request.form["email"]
-        senha = request.form["senha"]
+        senha = request.form["password"]
         try:
             cursor.execute("SELECT * FROM usuario WHERE Email = %s AND Senha = %s", (email, senha))
             usuario = cursor.fetchone()
