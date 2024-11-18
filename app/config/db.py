503b812c2080f73d0dac6fabcb7b257476b6519f
luizmatoso <luizhmatoso1340@gmail.com>
2024-11-18 18:07:46 -0300

    Real time data page styles

--- app/config/db.py
+++ app/config/db.py
@@ -5,7 +5,7 @@ db = SQLAlchemy()
 DB_HOST = "localhost"
 DB_NAME = "eBoi"
 DB_USER = "postgres"
-DB_PASS = "root"
+DB_PASS = "postgres"
 DB_PORT = "5432"
 
 SQLALCHEMY_DATABASE_URI = f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
